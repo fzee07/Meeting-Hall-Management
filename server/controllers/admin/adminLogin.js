c7549a731107568e3fb72d18a5355edcc05c4bd6
fzee007 <faizan.h@globalsinc.com>
2022-05-28 18:07:31 +0530

    1 part done

--- server/controllers/admin/adminLogin.js
+++ server/controllers/admin/adminLogin.js
@@ -1,3 +1,17 @@
-const adminLogin = async (req, res) => {};
+const Admin = require("../../models/Admin");
+
+const adminLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = Admin.findOne({ email: email });
+  if (user) {
+    res.status(404).json({
+      success: false,
+      message: "User Already Exists",
+    });
+  }else{
+    user.create({ user})
+  }
+};
 
 module.exports = { adminLogin };
